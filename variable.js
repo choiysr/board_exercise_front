0b1b9dc16b42d64bb0b67a7130ccd0a0302b30f7
valsa_de_euridice@naver.com <zkzkdh99%>
2020-04-09 18:32:21 +0900

    paging

---
+++ variable.js
@@ -0,0 +1,11 @@
+var item = (function () {
+
+    let $prevBtn = $("#zero_config_previous");
+    let $nextBtn = $("#zero_config_next");
+
+    return {
+        $prevBtn : $prevBtn,
+        $nextBtn : $nextBtn
+    }
+
+})();
